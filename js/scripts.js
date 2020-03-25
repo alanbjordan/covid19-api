8efa43a3df79b2ba3d4e8e60dde8e8b9c4659a27
alanbjordan <alan.jordanbiz@gmail.com>
2020-03-25 16:00:14 -0400

    updated files

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,23 @@
 "use strict";
 const stateChangeform = document.querySelector('#stateChangeForm');
 const eachState = document.querySelector('#state');
+
 const getStates = function getData(){
     const apiURL = `https://corona.lmao.ninja/states`;    
     const stateSelectLabel = document.querySelector('#stateSelectLabel');
+    const casesLabel = document.querySelector('#caseslabel');
     get(apiURL).then(function(response){
         console.log(response);
-            const stateElement = document.createElement('select');        
+            const stateElement = document.createElement('select');   
+            casesLabel.innerHTML = response[0].cases;                
         response.forEach(element => {
             const stateOption = document.createElement('option');
             stateOption.value = element.state;
             stateOption.text = element.state;    
-            stateElement.append(stateOption);    
+            stateElement.append(stateOption);   
         });
         stateSelectLabel.appendChild(stateElement);
+
     });
 }
-
 getStates();
